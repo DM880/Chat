01d80a0d8893007f9639f828cb01794dc7c06d00
DM880 <denys.mauro@gmail.com>
2022-03-05 13:44:42 +0000

    robot js fixed

--- src/chat_app/interfaces/appsite/static/js/create_room_chat.js
+++ src/chat_app/interfaces/appsite/static/js/create_room_chat.js
@@ -1,38 +1,65 @@
+// Info
+
 var info_menu = document.getElementById('info-menu');
 var info = document.getElementById('info');
 
-  info_menu.addEventListener('mouseover',function(){
-    info.style.display = 'block';
+info_menu.addEventListener('mouseover',function(){
+  info.style.display = 'block';
 });
 
-  info_menu.addEventListener('mouseleave',function(){
-    info.style.display = 'none';
+info_menu.addEventListener('mouseleave',function(){
+  info.style.display = 'none';
 });
 
 
-  info_menu.addEventListener('click',function(){
- if(info.style.display == 'block')
- {
-   info.style.display = 'none';
+info_menu.addEventListener('click',function(){
+  if(info.style.display == 'block')
+  {
+  info.style.display = 'none';
   }
   else{
     info.style.display = 'block';
   }
 });
 
+
+// Robot
+
 var robot = document.getElementById("robot");
-var robot_container = document.getElementById("robot-container");
 var robot_after = document.getElementById("robot_after");
 var my_info_container = document.getElementById("my-info-container");
 
-  robot.addEventListener("mouseover",function(){
-    robot_after.style.display = "block";
-    robot.style.display = "none";
-    my_info_container.style.display = "block";
+
+robot.addEventListener('click',function(){
+  if(robot.style.display == 'block')
+  {
+    robotAfter();
+    }
+  else{
+    robotBefore();
+  }
   });
 
-  robot_container.addEventListener("mouseleave",function(){
-    robot.style.display = "block";
-    robot_after.style.display = "none";
-    my_info_container.style.display = "none";
+
+robot_after.addEventListener('click',function(){
+  if(robot_after.style.display == 'block')
+  {
+    robotBefore();
+  }
+  else{
+    robotAfter();
+  }
   });
+
+
+function robotAfter(){
+  robot.style.display = 'none';
+  robot_after.style.display = "block";
+  my_info_container.style.display = "block";
+}
+
+function robotBefore(){
+  robot.style.display = "block";
+  robot_after.style.display = "none";
+  my_info_container.style.display = "none";
+}
